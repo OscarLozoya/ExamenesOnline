a50e9c59895b8dcb66e7ae16935458afb2b5c281
Bet0z <joses-007@hotmail.com>
2016-02-21 19:30:09 -0600

    Agrega la pagina VistaExamen. Se cambia el index a php. Se elimina el antiguo index.

---
+++ VistaExamen.php
@@ -0,0 +1,45 @@
+<?php include("Header.php"); ?>
+
+	<div class="container-fluid row">
+		<aside class="col-xs-12 col-sm-3 aside">
+			<fieldset>
+				<legend>Tiempo Restante</legend>
+				<h1>00:00</h1>
+			</fieldset>
+			<br>
+			<fieldset>
+				<legend>Panel Preguntas</legend>
+				<div class="table-responsive">
+					<table class="table table-bordered">
+						<tr>
+							<td><h6>1</h6></td>
+							<td><h6>2</h6></td>
+							<td><h6>3</h6></td>
+						</tr>
+						<tr>
+							<td><h6>4</h6></td>
+							<td><h6>5</h6></td>
+							<td><h6>6</h6></td>
+						</tr>	
+					</table>
+				</div>
+			</fieldset>
+		</aside>
+		<section class="col-xs-12 col-sm-8 content">
+			<article>
+				<h1>Categoria	No. Pregunta/Total preguntas</h1>
+			</article>
+			<article>
+				<div>
+					<p>Pregunta</p>
+				</div>
+				<div>
+					<p>Respuesta</p>
+				</div>
+				<footer>
+					<button class="btn btn-default">Siguiente</button>
+				</footer>
+			</article>	
+		</section>
+	</div>
+<?php include("Footer.php");?>
--- index.php
+++ index.php
@@ -1,21 +1,61 @@
 <?php include("Header.php"); ?>
-	<aside class="AsideUser">
-		<form action="." method="post">
-			<p>
-				<label>Usuario: </label>
-				<input type="text" name="user"/>
-				<br/>
-				Contraseña: <input type="date"/>
-			</p>	
-			<button type="submit" >NOPE</button>
-		</form>
-		<!--<figure class="ImgAvatar">
-			<img src="Imagenes/logo.png">
-			<figcaption>Imagen de Avatar sonriente</figcaption>
-		</figure>-->
-	</aside><!--
-<section>
-			<article>
-				</article>
-		</section>-->
+
+<?php include("Slider.php"); ?>
+
+		<aside class="col-xs-12 col-sm-4 aside">
+			<br>
+			<form action="" class="form-horizontal">
+				<div class="form-group row">
+					<div class="col-xs-3">
+						<label for="usuario" class="control-label">Usuario:</label>
+					</div>
+					<div class="col-xs-8">
+						<input class="form-control" id="usuario" type="text" placeholder="Usuario">
+					</div>					
+				</div>
+
+				<div class="form-group row">
+					<div class="col-xs-3">
+						<label for="contrasena" class="control-label">Contraseña:</label>
+					</div>	
+					<div class="col-xs-8">
+						<input class="form-control" id="contrasena" type="password" placeholder="Contraseña">
+					</div>
+				</div>
+
+				<div class="form-group container">
+					<button class="btn btn-link">¿Olvido su contraseña?</button>
+				</div>
+
+				<div class="form-group">
+					<div class="col-xs-5">
+						<button class="btn">Registrarse</button>
+					</div>
+					<div class="col-xs-3">
+						<button class="btn">Ingresar</button>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<button class="btn"><i class="fa fa-facebook-official"></i> Registrarse con Facebook</button>
+					<br>
+					<br>
+					
+					<button class="btn"><i class="fa fa-twitter-square"></i> Registrarse con Twitter</button>
+					
+					<br>
+					<br>
+					
+						<button class="btn"><i class="fa fa-google-plus-square"></i> Registrarse con Google Plus</button>
+					
+				</div>
+			</form>
+		</aside>
+
+		<article class="col-xs-12 col-sm-8 content">
+			<h1>Categorias</h1>
+		</article>
+
+	</section>
+
 <?php include("Footer.php");?>
