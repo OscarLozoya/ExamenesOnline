75eab84da2292ebaa735c83b542a560c0b9005db
Bet0z <joses-007@hotmail.com>
2016-02-22 23:30:06 -0600

    Se agrega la vista IndexUser, que sera la que el usuario vea cuando inicie sesión. CSS generales se le agrega una regla para poner un borde y que no se vean los bloques tan pegados.

---
+++ IndexUser.php
@@ -0,0 +1,42 @@
+<?php include("Header.php");?>
+<?php include("Slider.php");?>
+<secction class="container-fluid row">
+    <aside class="col-xs-12 col-sm-3 lines aside">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <figure>
+                    <img src="">
+                </figure>
+            </div>
+            <p>Nombre completo</p>
+            <a href="">Ver Perfil</a>
+        </div>
+        <p>Examenes Pendietes</p>
+        <form action="">
+            <select>
+                <option value="">Opcion1</option>
+                <option value="">Opcion2</option>
+                <option value="">Opcion3</option>
+                <option value="">Opcion4</option>
+            </select>
+            <p>Categoria</p>
+            <p>Fecha Limite</p>
+        <div class="form-group">
+            <button class="btn">Ir al Examen</button>
+            <button class="btn">Salir</button>
+        </div>
+        </form>
+    </aside>
+    <article class="col-xs-12 col-sm-6 content lines">
+            <h1>Categorias</h1>
+    </article>
+    <aside class="col-xs-12 col-sm-3 lines aside">
+        <form action="">
+            <input class="form-control" type="date">
+            <button class="btn btn-link">Ver Eventos Proximos</button>
+            <button class="btn btn-link">Detalles Examenes</button>
+        </form>
+    </aside>
+</section>
+<div class="clear-fix visible-sm-block"></div>
+<?php include("Footer.php"):?>
