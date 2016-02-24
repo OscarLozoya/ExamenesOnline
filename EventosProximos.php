0d9fb1b44f8eb72f822484f2caf0a974d7dbe0d2
Gerardo Perez <yaalidream@hotmail.com>
2016-02-23 23:33:16 -0600

    Se agrego la pagina EventosProximos.php

---
+++ EventosProximos.php
@@ -0,0 +1,38 @@
+<?php include("Header.php"); ?>
+<?php include("Slider.php"); ?>
+	<section class="container-fluid">
+		<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+			<div class="jumbotron">
+				<div class="form-horizontal well">
+					<div class="form-group">
+							<label class="col-md-3">Categoría</label>
+							<label class="col-md-3">Nombre Exámen</label>
+							<label class="col-md-3">Preguntas 00</label>
+							<button class="btn">Ir al Exámen</button>
+							<button class="btn btn-danger">Reprobar</button>
+					</div>
+					<div class="form-group">
+							<label class="col-md-3">Fecha Limite</label>
+							<label class="col-md-3">Tiempo</label>
+					</div>
+				</div>
+				<div class="form-horizontal well">
+					<div class="form-group">
+							<label class="col-md-3">Categoría</label>
+							<label class="col-md-3">Nombre Exámen</label>
+							<label class="col-md-3">Preguntas 00</label>
+							<button class="btn">Ir al Exámen</button>
+							<button class="btn btn-danger">Reprobar</button>
+					</div>
+					<div class="form-group">
+							<label class="col-md-3">Fecha Limite</label>
+							<label class="col-md-3">Tiempo</label>
+					</div>
+				</div>
+				<div class="col-md-offset-6">
+					<button class="btn btn-warning btn-large">Volver</button>
+				</div>
+			</div>
+		</article>
+	</section>
+<?php include("Footer.php");?>
