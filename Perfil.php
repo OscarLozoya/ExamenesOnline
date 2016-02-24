ee9204bcdfcd9c9cb65af4d2c0eb47dba2e6a5c8
Gerardo Perez <yaalidream@hotmail.com>
2016-02-24 00:07:33 -0600

    Se agrego el archivo perfil.php con su estructura y sus respectivos formularios

---
+++ Perfil.php
@@ -0,0 +1,90 @@
+<?php include("Header.php"); ?>
+	<section class="container-fluid">
+		<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+			<div class="jumbotron">
+				<h2 class="text-center">Nombre Del Usuario</h2>
+				<form>
+					<div class="form-horizontal well">
+						<h3>Datos Personales</h3>
+						<div class="form-group">
+								<label class="col-md-2">Nombre</label>
+								<label class="col-md-2">Apellido Paterno</label>
+								<label class="col-md-2">Apellido Materno</label>
+								<label class="col-md-2">Telefono Fijo</label>
+								<label class="col-md-2">Telefono Celular</label>
+						</div>
+						<div class="form-group">
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+						</div>
+						<div class="form-group">
+								<label class="col-md-2 control-label">Correo</label>
+								<div class="col-md-4"><input class="form-control"></input></div>
+						</div>
+					</div>
+					<div class="form-horizontal well">
+						<h3>Redes sociales</h3>
+						<div class="form-group">
+								<label class="col-md-2">Nombre</label>
+								<label class="col-md-2">Apellido Paterno</label>
+								<label class="col-md-2">Apellido Materno</label>
+								<label class="col-md-2">Telefono Fijo</label>
+								<label class="col-md-2">Telefono Celular</label>
+						</div>
+						<div class="form-group">
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+						</div>
+						<div class="form-group">
+								<label class="col-md-2 control-label">Correo</label>
+								<div class="col-md-4"><input class="form-control"></input></div>
+						</div>
+					</div>
+					<div class="form-horizontal well">
+						<h3>Datos Académicos</h3>
+						<div class="form-group">
+								<label class="col-md-2">Nombre</label>
+								<label class="col-md-2">Apellido Paterno</label>
+								<label class="col-md-2">Apellido Materno</label>
+								<label class="col-md-2">Telefono Fijo</label>
+								<label class="col-md-2">Telefono Celular</label>
+						</div>
+						<div class="form-group">
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+								<div class="col-md-2"><input class="form-control"></input></div>
+						</div>
+						<div class="form-group">
+								<label class="col-md-2 control-label">Correo</label>
+								<div class="col-md-4"><input class="form-control"></input></div>
+						</div>
+					</div>
+					<div class="form-horizontal well ">
+						<h3>Cambiar Contraseña</h3>
+						<div class="form-group">
+								<label class="col-md-3">Contraseña Actual</label>
+								<label class="col-md-3">Nueva Contraseña</label>
+								<label class="col-md-3">Confirmar</label>
+						</div>
+						<div class="form-group">
+								<div class="col-md-3"><input type="password" class="form-control"></input></div>
+								<div class="col-md-3"><input type="password" class="form-control"></input></div>
+								<div class="col-md-3"><input type="password" class="form-control"></input></div>
+						</div>
+					</div>
+					<div class="col-md-offset-5 col-md-2">
+						<button class="btn btn-primary btn form-control">Actualizar</button>
+					</div>
+				</form>
+			</div>
+		</article>
+	</section>
+<?php include("Footer.php");?>
