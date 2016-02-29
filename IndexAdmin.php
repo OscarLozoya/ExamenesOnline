8b281f805bf537cc93f30562709f6c5ae78273bd
Bet0z <joses-007@hotmail.com>
2016-02-28 21:04:21 -0600

    Se Actualiza la versión del maquetado de Balsamiq Mockups, agregando la vista para que el admin de de alta un usuario. Se agrega el Index para el Admin y el Mod. Como los menus para estos usuarios son diferentes y se van a repetir en el resto de sus paginas, se agrega MenuAdmin y menuMod para ser incluidos en sus paginas.

---
+++ IndexAdmin.php
@@ -0,0 +1,27 @@
+<?php include("Header.php"); ?>
+<div class="form-group">
+	<label class="label-control">Historial de Navegación</label>
+</div>
+<?php include("MenuAdmin.php"); ?>
+	<section class="container-fluid">
+		<aside class="col-xs-12 col-sm-4 aside lines">
+			<div class="form-group">
+				<div class="col-xs-12">
+					<figure>
+	                    <img src="img">
+	                </figure>
+				</div>
+				<div class="col-xs-12">
+					<label class="control-label">Nombre completo</label>
+				</div>
+				<div class="col-xs-12">
+					<a href="">Ver Perfil</a>
+				</div>
+			</div>
+		</aside>
+
+		<article class="col-xs-12 col-sm-8 content lines">
+            <h1>Categorias</h1>
+    </article>
+	</section>
+<?php include("Footer.php"); ?>
---
+++ MenuMod.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>                        
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li><a href="#">Inicio</a></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Preguntas <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Nueva</a></li>
+              <li><a href="#">Modificar</a></li>
+              <li><a href="#">Eliminar</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Examenes <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Nuevo</a></li>
+              <li><a href="#">Modificar</a></li>
+              <li><a href="#">Eliminar</a></li>
+            </ul>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+              <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Salir</a></li>
+        </ul> 
+      </div>
+    </div>
+  </nav>
