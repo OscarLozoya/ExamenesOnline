7efcda02b96acb4dcb9cd410b078a858638c224a
Oscar Lozoya <lozoya.oscar.ivan@gmail.com>
2016-03-07 19:40:53 -0600

    Se cambio el contenido del footer y se puso un contenido y estilo  previo

--- Footer.php
+++ Footer.php
@@ -1,8 +1,11 @@
 <!--Se cierra el contenedor que tiene desde el Header (debajo del<body>) hasta antes del footer-->
 </aside>
 <footer id="footer" class="Footer">
-		<div class="container">
-			<h1>Footer</h1>
+		<div class="container Formato-Footer">
+			<br>
+			<p>Desarrollado por DeadDevelopers
+				<br>
+				Dudas o aclaraciones envienos un correo a deadDevelopers@gmail.com</p>
 		</div>
 </footer>
 </body>
