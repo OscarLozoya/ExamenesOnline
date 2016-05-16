22d2ef37bf07ba965067dc462792187c2684cf95
Oscar Lozoya <lozoya.oscar.ivan@gmail.com>
2016-05-16 12:39:40 -0500

    Informacion para Conexiones y pagina 404
    Se agrego un archivo de prueba para realizar la conexion a una base de datos.
    Se modifico el archivo examenCtl.php alli se encuentra informacion complementaria de la comunicacion con la Base de datos.
    Se creeo la pagina 404 y se añadio un archivo de imagen para la misma.
    Se eliminaron archivos no necesarios.

--- 404.php
+++ 404.php
@@ -3,6 +3,6 @@
 <br>
 <br>
 <div class="col-xs-12 col-md-6 col-md-offset-3">
-	<img src="image_404.png" alt="Ponga un 100 por favor">
+	<img src="images/image_404.png" alt="Ponga un 100 por favor">
 </div>
 <?php include("app/vistas/Footer.php"); ?>
--- app/controladores/examenCtl.php
+++ app/controladores/examenCtl.php
@@ -60,8 +60,35 @@
 		*/
 		function vista()
 		{
-			if(empty($_POST))
-				require_once('app/vistas/VistaExamen.php');
+			/*if(empty($_POST))
+				require_once('app/vistas/VistaExamen.php');*/
+
+			$vista = file_get_contents("app/vistas/VistaExamen.php");
+			$footer = file_get_contents("app/vistas/Footer.php");
+			$inicia_fila = strrpos($vista,'<tr>');
+			$termina_fila = strrpos($vista,'</tr>')+5;
+			$fila = substr($vista,$inicia_fila,$termina_fila-$inicia_fila);
+
+			$Diccionario = array(
+				'{Categoria}' => 'POO',
+				'{No. Pregunta}' => '5',
+				'{Total preguntas}' => '8',
+				'{Pregunta}' => '¿Nos va a Pasar?',
+				'{Respuesta}' => '<form><input type="radio">Si<br><input type="radio">No<br></form>',
+				 );
+			for ($i=1; $i <=9; $i+=3) { 
+				$new_fila = $fila;
+				$Diccionario_Tabla = array(
+					'{Pregunta 1}' => $i,
+					'{Pregunta 2}' => $i+1,
+					'{Pregunta 3}' => $i+2, );
+				$new_fila = strtr($new_fila, $Diccionario_Tabla);
+				$filas.=$new_fila;
+			}
+			$vista= strtr($vista,$Diccionario);
+			$vista= str_replace($fila,$filas,$vista);
+
+			echo $vista.$footer;	
 		}
 
 	}
